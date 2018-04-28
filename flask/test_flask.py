d3b7f19401726e3a0fc79e6f84563fcda1080954
FerreWagner <18408229270@163.com>
2018-04-28 11:49:32 +0800

    test route

--- flask/test_flask.py
+++ flask/test_flask.py
@@ -1,9 +1,20 @@
 from flask import Flask
 app = Flask(__name__)
 
+# @app.route('/')
+# def hello_world():
+#     return 'Hello World!'
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'hello ferre'
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
