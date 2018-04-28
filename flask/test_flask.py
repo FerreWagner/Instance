0fdeec03790313ce0a04c3d72f1026d39fd1c2ff
FerreWagner <18408229270@163.com>
2018-04-28 14:49:32 +0800

    test route 2

--- flask/test_flask.py
+++ flask/test_flask.py
@@ -1,6 +1,7 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, url_for
 
+app = Flask(__name__)
+# 你可以用 url_for() 来给指定的函数构造 URL。它接受函数名作为第一个参数，也接受对应 URL 规则的变量部分的命名参数。未知变量部分会添加到 URL 末尾作为查询参数
 # @app.route('/')
 # def hello_world():
 #     return 'Hello World!'
@@ -11,6 +12,24 @@ def index():
 @app.route('/hello')
 def hello():
     return 'hello ferre'
+@app.route('/user/<username>')
+
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+@app.route('/about')
+def reek():
+    return 'The about page'
+
+# with app.test_request_context():
+#     print(url_for('login'))     #/login
+    # print(url_for('profile', username='John Doe'))      #/user/John%20Doe
 
 if __name__ == '__main__':
     app.run(debug=True)
