f78eae31d2ca3160970c2380a107d7a3fce12295
FerreWagner <18408229270@163.com>
2018-05-09 16:37:43 +0800

    file base attr success

--- case/Os_use.py
+++ case/Os_use.py
@@ -1,8 +1,14 @@
 import os
 
-
-def file_attr(path):
-    f = os.listdir(path)
+# 文件基本属性
+def file_attr(file_name):
+    if os.path.isfile(file_name):
+        content = os.stat(file_name)
+        print('文件大小为：', content.st_size)
+        print('上次访问时间：', content.st_atime)
+        print('最后修改时间：', content.st_mtime)
+    else:
+        print('不是一个文件路径')
     # for i in f:
     #     print(i)
     #     if os.path.isdir(i) == True:
@@ -12,4 +18,4 @@ def file_attr(path):
 
     # print(os.stat(path))
 
-file_attr('D:/todo/')
+file_attr('D:/todo/123.txt')
